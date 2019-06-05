e6436e0f8ea4ba54b058bd7668ea344a8cce586f
Sean Choi <ss.sean.choi@gmail.com>
2019-06-05 02:26:35 -0400

    finished modelling and testing codes

--- helper_module.py
+++ helper_module.py
@@ -5,11 +5,12 @@ from keras import backend as K
 
 from sklearn.metrics import confusion_matrix
 
-def training_plot(hist):
+def training_plot(hist,model_name):
     loss = [hist.history['loss'],hist.history['val_loss']]
     acc = [hist.history['acc'],hist.history['val_acc']]
     
     fig, axes = plt.subplots(1,2,figsize = (15,3))
+    fig.suptitle(model_name)
     for ax,val,name in zip(axes,(loss,acc),['loss','accuracy']):
         ax.plot(val[0], color='b', label="Training")
         ax.plot(val[1], color='r', label="Validation")
